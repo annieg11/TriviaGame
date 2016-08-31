6924389a5daf86f87ce0052fec978546e442b741
aanchal <aanchal.g.gupta@gmail.com>
2016-08-31 16:57:00 -0400

    Changes made to js file and image folder

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # TriviaGame
+
+Challenges faced:
+
+1.To display the next question,even after iterating through the questions.
+
+solution: Made a function to show question area and to call the code increment question.
+
+2.To set the time interval so that it automatically moves on to the next question.
+
+solution: Had to combine it with hide content area function,as the question is hidden, it displays messge for 3 sec and the moves on to the next question.
+
+3. Last question was getting displayed again after finishing the increment loop,had a tough time in Reseting the game.
+
+Solution: Called the Restart function after the on click function which starts onclicking the start button.
+
+
+
+Overall, a good learning experience.
+
+Areas of improvement:
+
+To set the timer function  30 sec to perfection, whenever we move on to the next question.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,147 +1,142 @@
 
-$(document).ready(function () {
-
-
-// to make an array which stores questions.
-var questions =[{
- question:'Who invented the BALLPOINT PEN?',
- choices:['Biro Brothers', 'Waterman Brothers', ' Bicc Brothers','Write Brothers'],
- correctAnswer:0
-},{
-question:'Which scientist discovered the radioactive element radium?',
-choices:['Isaac Newton', 'Albert Einstein', 'Benjamin Franklin', 'Marie Curie'],
-correctAnswer:3
-},{
-question: 'What Galileo invented?',
-choices:['Barometer', 'Pendulum clock', 'Microscope', 'Telescope'],
-correctAnswer:3
-},{
-question:'What Benjamin Franklin invented?',
-choices:['Bifocal spectacles','Radio','Barometer','Hygrometer'],
-correctAnswer:0
-},{
-question:'What Elisha Otis invented?',
-choices:['The brake used in modern elevators','Jet Engine','Turbine','Hydraulic Crane'],
-correctAnswer:0
-}];
- // alert(questions.length);
-var currentQuestion = 0;
-var correctAnswers = 0;
-var quizOver = false;
-
-
-
-  // This displays the current question AND the choices
-function displayCurrentQuestion() {
-
-    console.log("In display current Question");
-
-    var askquestion = questions[currentQuestion].question;
-    var questionClass = $(document).find(".container > .question");
-    var choiceList = $(document).find(".container > .choiceList");
-    var numChoices = questions[currentQuestion].choices.length;
-    
-
-    // Set the questionClass text to the current question
-    $(questionClass).text(askquestion);
-      console.log(askquestion);
-    // Remove all current <li> elements (if any)
-    // $(choiceList).find("li").remove();
-
-    var choice;
-    for (i = 0; i < numChoices; i++) {
-        choice = questions[currentQuestion].choices[i];
-        $('<li><input type="radio" value=' + i + ' name="dynradio" />' + choice + '</li>').appendTo(choiceList);
+$(document).ready(function() {
+  // to make an array which stores questions,its choices and correct answer index.
+  var questions = [{
+      question: 'Who invented the BALLPOINT PEN?',
+      choices: ['Biro Brothers', 'Waterman Brothers', ' Bicc Brothers', 'Write Brothers'],
+      correctAnswer: 0
+    }, {
+      question: 'Which scientist discovered the radioactive element radium?',
+      choices: ['Isaac Newton', 'Albert Einstein', 'Benjamin Franklin', 'Marie Curie'],
+      correctAnswer: 3
+    }, {
+      question: 'What Galileo invented?',
+      choices: ['Barometer', 'Pendulum clock', 'Microscope', 'Telescope'],
+      correctAnswer: 3
+    }, {
+      question: 'What Benjamin Franklin invented?',
+      choices: ['Bifocal spectacles', 'Radio', 'Barometer', 'Hygrometer'],
+      correctAnswer: 0
+    }, {
+      question: 'What Elisha Otis invented?',
+      choices: ['The brake used in modern elevators', 'Jet Engine', 'Turbine', 'Hydraulic Crane'],
+      correctAnswer: 0
+    }]
+    // alert(questions.length);
+  var currentQuestion = 0;
+  var correctAnswers = 0;
+  var count = 0;
+  var counter;
+  // Setting timer function to call time remaining and decrease count by 1sec.
+  function timer() {
+    $(".timeleft").text("Time Remaining: " + count + " secs");
+    count--;
+    if (count === 0) {
+      // if count is 0, then clear the time interval.
+      clearInterval(counter);
+      // We are getting the correct answer text from this variable.
+      var temp = questions[currentQuestion].correctAnswer;
+      // If user input is not defined,then it flashes this message in the message div.
+      $(".Message").text("Time up!!" + "Correct Answer is " + questions[currentQuestion].choices[temp]);
+      // We move on to the next question.
+      currentQuestion++;
+      // We hide the qestions,choicelist and time remaining to show our messge text and then after 2 sec time interval,we show them back.
+      hideContentArea();
     }
-}
-function displayScore() {
-    $(document).find(".container > .result").text("You scored: " + correctAnswers + " out of: " + questions.length);
-    $(document).find(".container > .result").show();
-}
-function hideScore() {
-    $(document).find(".result").hide();
-}
-function resetQuiz() {
+  }
+
+  function showQuestionArea() {
+    $(".Message").hide();
+    // if at the current question then we call display the question function which displays the current question and its choicelist.
+    if (currentQuestion < questions.length) {
+      displayCurrentQuestion();
+      // console.log(currentQuestion);
+    } else {
+      // if no more questions to ask then,it displays score.
+      displayScore();
+      // replacing 'start' div with 'start again?'.And showing it.
+      $(".startButton").text("Start Again?");
+      $(".startButton").show();
+    }
+  }
+  // Restart the game,we hide result and initialize the variables to 0.
+  function restart() {
+    $(".result").hide();
     currentQuestion = 0;
     correctAnswers = 0;
-    hideScore();
-    $(document).find(".timeleft").hide();
-}
-    // Display the first question  
-    $(document).find(".timeleft").hide();
-    // On clicking next, display the next question
-    $(this).find(".startButton").on("click", function () {
-          $(document).find(".startButton").hide();
-          $(document).find(".timeleft").show();
-          var count=30;
-      function timer()
-      {
-        count--;
-        $(document).find(".timeleft").text("Time Remaining: " + count + " secs");
-          if (count === 0)
-          {
-         clearInterval(counter);
-         //alert('Time Up!');
-          }
-    }  
-    
-       displayCurrentQuestion();
-      var counter=setInterval(timer, 1000); 
-           
-    });
-        if (!quizOver) {  
-            $(this).find(".choiceList").on("click", function () {
-              var userinput=$("input[type='radio']:checked").val();
-                 console.log("value="+userinput);
-                 console.log("correctAnswer="+questions[currentQuestion].correctAnswer); 
-                if (userinput == questions[currentQuestion].correctAnswer) {
-                    correctAnswers++;
-                    $(document).find(".timeleft").hide();
-                     $(document).find(".question").hide();
-                     $(document).find(".choiceList").hide();
-                    $(document).find(".Message").text("Correct answer!!");
-                    $(document).find(".Message").show();
-                    setTimeout(fourSeconds,4000);
-                      function fourSeconds(){
-                     displayCurrentQuestion();
-
-                   }
-                    
-                } else{
-                  $(document).find(".timeleft").hide();
-                  var temp=questions[currentQuestion].correctAnswer;
-                  $(document).find(".Message").text("Wrong answer!!"+ "Correct Answer is "+ questions[currentQuestion].choices[temp]);
-                   $(document).find(".question").hide();
-                  $(document).find(".choiceList").hide();
-                    
-                };            
-                if (currentQuestion < questions.length) {
-                       displayCurrentQuestion();
-                    console.log(currentQuestion);        
-                    timer();
-                } else {
-                    displayScore();
-                    //                    $(document).find(".nextButton").toggle();
-                    //                    $(document).find(".playAgainButton").toggle();
-                    // Change the text in the next button to ask if user wants to play again
-                    $(document).find(".starttButton").text("Start Again?");
-                    quizOver = true;
-                }
-           });
-           } 
-         else { // quiz is over and clicked the next button (which now displays 'Play Again?'
-            quizOver = false;
-            $(document).find(".startButton").text("Start");
-            resetQuiz();
-            time();
-            displayCurrentQuestion();
-            hideScore();
-        };
-     
-    });
-
-
-
-
-
-
+  }
+  // this displays the correct score out of number of questions asked.
+  function displayScore() {
+    $(".result").text("You scored: " + correctAnswers + " out of: " + questions.length);
+    $(".result").show();
+  }
+  // This displays the current question AND the choices.
+  function displayCurrentQuestion() {
+    // console.log("In display current Question");
+    // Decrementing 30 secs by 1 sec till the time is up.Then it moves on to the next question.
+    count = 30;
+    counter = setInterval(timer, 1000);
+    $(".timeleft").show();
+    $(".question").show();
+    $(".choiceList").show();
+    var askquestion = questions[currentQuestion].question;
+    // setting variables for the classes on the divs respectively.
+    var questionClass = $(".question");
+    var choiceList = $(".choiceList");
+    // variable set to get the choices length for the current question asked.
+    var numChoices = questions[currentQuestion].choices.length;
+    // Set the questionClass text to the current question
+    $(questionClass).text(askquestion);
+    //console.log(askquestion);
+    // To show choices of the question asked.
+    var choice;
+    for (i = 0; i < numChoices; i++) {
+      choice = questions[currentQuestion].choices[i];
+      // displaying the choices in the choicelist div.
+      $('<li><input type="radio" value=' + i + ' name="dynradio" />' + choice + '</li>').appendTo(choiceList);
+    }
+  }
+  // to hide the content area so that we can display the message  for 2sec,hence setting the time interval in this function.
+  function hideContentArea() {
+    $(".timeleft").hide();
+    $(".question").hide();
+    $(".choiceList").hide();
+    $(".Message").show();
+    $('.choiceList').empty();
+    setTimeout(function() {
+      showQuestionArea();
+    }, 3000);
+  }
+  // To hide the timeleft div before the start of the game.
+  $(".timeleft").hide();
+  // On clicking next, display the next question
+  $(this).find(".startButton").on("click", function() {
+    // Hide start button
+    $(".startButton").hide();
+    // restart function is called here so that once the cycle finishes,on clicking the start button we could start again, the game.
+    restart();
+    // display function is called here so that we can display the question and its choices.
+    displayCurrentQuestion();
+    // end of on cilick function here.
+  });
+  // an on click function is used here to capture the user input to the radio button.
+  $(this).find(".choiceList").on("click", function() {
+    clearInterval(counter);
+    var userinput = $("input[type='radio']:checked").val();
+    // console.log("value=" + userinput);
+    // console.log("correctAnswer=" + questions[currentQuestion].correctAnswer);
+    //  loop to know if user input is correct or wrong,  hence displaying the message accordingly.
+    if (userinput == questions[currentQuestion].correctAnswer) {
+      correctAnswers++;
+      $(".Message").text("Correct answer!!");
+       $('<img src="assets/images/happy.jpg">').appendTo(".Message");
+    } else {
+      var temp = questions[currentQuestion].correctAnswer;
+      $(".Message").text("Wrong answer!!" + "Correct Answer is " + questions[currentQuestion].choices[temp]);
+      $("<img src='assets/images/betterluck.jpg'>").appendTo(".Message");
+    };
+    currentQuestion++;
+    // clearing the content area.
+    hideContentArea();
+  });
+}); 
